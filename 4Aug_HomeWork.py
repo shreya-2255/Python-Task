7f397439d09c1b8768cdb25c15d5ee4ea2718a8a
shreya-2255 <shreya.g2255@gmail.com>
2026-08-05 14:09:01 +0530

    Add files via upload

---
+++ 4Aug_HomeWork.py
@@ -0,0 +1,41 @@
+# Task 1: Identity Card
+name = "Smruti"
+age = 21
+city = "Pune"
+dob = "22/11/2004"
+college = "TKA"
+blood_group = "A+"
+
+print("----- My ID Card -----")
+print("Name =",name)
+print("Age =",age)
+print('City = "'+ city + '"')
+print("dob = ", dob)
+print('College = "' + college + '"')
+print("Blood Group = ", blood_group)
+
+print(" ")
+# Task 2: Find Data Type
+
+a =  25
+b = 25.5
+c = "twenty Five"
+d = True
+e = 2 + 3j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e)) 
+
+print(" ")
+# Task 3: Memory Detective
+
+v1 = 100
+v2 = 200
+v3 = 100
+
+print(id(v1))
+print(id(v2))
+print(id(v3))
